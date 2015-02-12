9a6578e189260339496e78fa5dfd03a6ced0221e
Sash Zats <sash@zats.io>
2015-02-12 09:40:44 -0500

    Initial prototype

---
+++ Leaks/LeaksInstrument.m
@@ -0,0 +1,139 @@
+//
+//  LeaksInstrument.m
+//  Leaks
+//
+//  Created by Sash Zats on 2/12/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+#import "LeaksInstrument.h"
+
+#import "LeaksInspector.h"
+#import "LeaksInspectorContext.h"
+#import "LeaksSession.h"
+
+
+static const NSPointerFunctionsOptions options = NSPointerFunctionsStrongMemory | NSPointerFunctionsObjectPersonality;
+
+
+@interface LeaksSession ()
+@property (nonatomic) NSHashTable *leaks;
+@property (nonatomic) NSUInteger index;
+@end
+
+
+@interface LeaksInstrument ()
+@property (nonatomic) LeaksInspector *inspector;
+@property (nonatomic) NSMutableArray *storage;
+@property (nonatomic) NSHashTable *cumulativeLeaks;
+@end
+
+
+@implementation LeaksInstrument
+
++ (instancetype)sharedInstrument {
+    static LeaksInstrument *instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[LeaksInstrument alloc] init];
+    });
+    return instance;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    self.inspector = [LeaksInspector new];
+    self.storage = [NSMutableArray array];
+    self.cumulativeLeaks = [NSHashTable weakObjectsHashTable];
+    return self;
+}
+
+#pragma mark - Public
+
+- (void)measure {
+    NSHashTable *currentRun = [self _aliveInstancesFromMyBundleExcludingMyClasses];
+    NSHashTable *previousRun = self.storage.lastObject;
+    if (previousRun) {
+        NSHashTable *delta = [currentRun copy];
+        [delta minusHashTable:self.cumulativeLeaks];
+        currentRun = delta;
+    }
+    LeaksSession *session = [LeaksSession new];
+    session.leaks = currentRun;
+    session.index = self.storage.count;
+    
+    for (id obj in session.leaks) [self.cumulativeLeaks addObject:obj];
+    
+    [self.storage addObject:session];
+}
+
+- (void)reset {
+    [self.storage removeAllObjects];
+}
+
+#pragma mark - Properties
+
+- (NSUInteger)count {
+    return self.storage.count;
+}
+
+- (NSArray *)allSession {
+    return [self.storage copy];
+}
+
+- (NSArray *)representativeSessions {
+    if (self.storage.count > 2) {
+        NSArray *result = [self.storage subarrayWithRange:NSMakeRange(1, self.storage.count - 2)];;
+        return result;
+    }
+    return nil;
+}
+
+- (NSHashTable *)cumulativeLeaksFromRepresentativeSessions {
+    NSHashTable *result = [[NSHashTable alloc] initWithOptions:options capacity:0];
+    for (LeaksSession *session in self.representativeSessions) {
+        for (id obj in session.leaks) {
+            [result addObject:obj];
+        }
+    }
+    return result;
+}
+
+- (BOOL)hasLeaksInRepresentativeSession {
+    for (LeaksSession *session in self.representativeSessions) {
+        if (session.leaks.count) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+#pragma mark - Private
+
+- (NSHashTable *)_aliveInstancesFromMyBundleExcludingMyClasses {
+    // our classes are still going to be around to measure stuff, ignore them
+    static NSSet *blacklistedClasses;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        blacklistedClasses = [NSSet setWithObjects:[LeaksInstrument class], [LeaksSession class], [LeaksInspector class], [LeaksInspectorContext class], nil];
+    });
+
+    NSHashTable *result = [[NSHashTable alloc] initWithOptions:options capacity:0];
+    [self.inspector enumerateAllInstancesUsingBlock:^(__unsafe_unretained Class cls, void *instance, BOOL *stop) {
+        if ([blacklistedClasses containsObject:cls]) {
+            return;
+        }
+        // Only classes from our app.
+        // TODO: will not work for frameworks?
+        if ([[NSBundle bundleForClass:cls] isEqual:[NSBundle mainBundle]]) {
+            [result addObject:(__bridge id)instance];
+        }
+    }];
+    
+    return result;
+}
+
+@end
---
+++ LeaksTests/LeaksTests.m
@@ -0,0 +1,94 @@
+//
+//  LeaksTests.m
+//  Leaks
+//
+//  Created by Sash Zats on 2/12/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "RootViewController.h"
+#import "LeaksInstrument.h"
+
+
+@interface LeaksTests : XCTestCase
+@property (nonatomic) LeaksInstrument *instrument;
+@end
+
+
+@implementation LeaksTests
+
+- (void)setUp {
+    [super setUp];
+    
+    self.instrument = [[LeaksInstrument alloc] init];
+}
+
+- (void)tearDown {
+    // to make sure we are not holding leaked instances from the previous test.
+    // Not that it matter if you use -[LeaksInstrument representativeSessions]
+    self.instrument = nil;
+ 
+    [super tearDown];
+}
+
+- (void)testFailingLeakingExample {
+    XCTestExpectation *leaksExpectation = [self expectationWithDescription:@"No leaks detected"];
+    
+    [self _pushPopViewControllerNTimes:4 leak:YES progressHandler:^{
+        [self.instrument measure];
+    } completionHandler:^{
+        XCTAssertFalse(self.instrument.hasLeaksInRepresentativeSession, @"%@", [self.instrument.representativeSessions componentsJoinedByString:@"\n"]);
+        [leaksExpectation fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:10 handler:nil];
+}
+
+- (void)testPassingNotLeakingExample {
+    XCTestExpectation *leaksExpectation = [self expectationWithDescription:@"No leaks detected"];
+    
+    [self _pushPopViewControllerNTimes:4 leak:NO progressHandler:^{
+        [self.instrument measure];
+    } completionHandler:^{
+        XCTAssertFalse(self.instrument.hasLeaksInRepresentativeSession, @"%@", [self.instrument.representativeSessions componentsJoinedByString:@"\n"]);
+        [leaksExpectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:nil];
+}
+
+- (void)_pushPopViewControllerNTimes:(NSUInteger)nTimes leak:(BOOL)leak progressHandler:(void(^)(void))progress completionHandler:(void(^)(void))handler {
+    if (!nTimes) {
+        handler();
+        return;
+    }
+
+    [self _pushPopViewControllerLeak:leak withCompletionHandler:^{
+        if (nTimes > 0) {
+            progress();
+        }
+        [self _pushPopViewControllerNTimes:nTimes - 1 leak:leak progressHandler:progress completionHandler:handler];
+    }];
+}
+
+- (void)_pushPopViewControllerLeak:(BOOL)leak withCompletionHandler:(void(^)(void))handler {
+    UINavigationController *navigationController = (UINavigationController *)[UIApplication sharedApplication].keyWindow.rootViewController;
+    RootViewController *rootViewController = navigationController.viewControllers.firstObject;
+    if (leak) {
+        [rootViewController pushLeakingViewController];
+    } else {
+        [rootViewController pushNotLeakingViewController];
+    }
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [rootViewController.navigationController popToViewController:rootViewController
+                                                            animated:YES];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            handler();
+        });
+    });
+}
+
+@end
